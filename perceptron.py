73b4076cf4cd9a909c37781b37e1522c936cd37b
hdbreaker <hdbreaker96@gmail.com>
2017-10-22 15:05:49 -0300

    Rename Functions

--- perceptron.py
+++ perceptron.py
@@ -8,7 +8,7 @@ class Perceptron():
 		self.learning_speed = 0.02
 		self.input_weights = []
 		self.expected_value = inputs[0]
-		self.bias = 1 # => Constant necessary for a better result prediction
+		self.bias = 1 # => Constant necessary for take a better decision 
 		self.init_weight()
 		
 	# Initial weigths for all the inputs
@@ -18,20 +18,20 @@ class Perceptron():
 			self.input_weights.append(random.random())
 
 	# Modify the weights based on the weighted sumatory of the inputs
-	def predict(self):
-		predict_result = 0
+	def calculate_decision(self):
+		decision = 0
 		counter = 0
 		for input in self.inputs:
-			predict_result += input * self.input_weights[counter]
+			decision += input * self.input_weights[counter]
 			counter += 1
-		self.evaluate(predict_result)
+		self.activate(decision)
 
 	# Detect if the expected result was accomplished, if it happened I will reinforce the weights in order to start the learning process
-	def evaluate(self, predict_result):
-		if(predict_result >= self.expected_value):
+	def activate(self, decision):
+		if(decision >= self.expected_value):
 			print "In the Light"
-			# If the prediction is correct, I will reinforce the weights based on the delta between the expected and the getted value, taking in consideration the learn speed variable
-			delta_error = (predict_result - self.expected_value)
+			# If the decision is the expected, I will reinforce the weights based on the delta between the expected and the getted value, taking in consideration the learn speed variable
+			delta_error = (decision - self.expected_value)
 			for weight in self.input_weights:
 				weight = weight * delta_error * self.learning_speed
 		else:
@@ -54,7 +54,7 @@ def training():
 			sensor_input[0] = sensor_input[1]
 			sensor_input[1] = sensor_input[1]+10
 		perceptron.set_inputs(sensor_input)
-		perceptron.predict()
+		perceptron.calculate_decision()
  	
 # Main loop to detect the differences between the last light input value and the new one after that the robot moves to the light
 def main():
@@ -69,7 +69,7 @@ def main():
 			sensor_input[0] = sensor_input[1]
 			sensor_input[1] = int(raw_input("New Input: "))
 			perceptron.set_inputs(sensor_input)
- 		perceptron.predict()
+ 		perceptron.calculate_decision()
  		counter += 1
 
 training()
